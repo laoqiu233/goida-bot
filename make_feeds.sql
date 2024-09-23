33da90c9a4b6547b2d81e2b12e991b8cb4457611
Dmitri Tsiu <dmitrimeowsalot@gmail.com>
2024-09-23 23:00:56 +0300

    Use postgres

--- make_feeds.sql
+++ make_feeds.sql
@@ -1,3 +1,5 @@
+DROP SCHEMA private_gpt CASCADE;
+
 TRUNCATE feeds CASCADE;
 INSERT INTO feeds(id, token, feed_name, slug, main_url, feed_url, is_active) VALUES 
 (gen_random_uuid(), 0, 'Лента.ру', 'lenta', 'https://lenta.ru/', 'https://lenta.ru/rss', true),
